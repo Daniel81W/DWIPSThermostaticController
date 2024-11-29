a60ab1183614313effc31301ab8ddea3dab28777
Daniel81W <it@dan-web.de>
2024-11-29 14:31:26 -0500

    Upd

--- DWIPSThermostaticController/module.php
+++ DWIPSThermostaticController/module.php
@@ -1,23 +1,38 @@
 <?php
 
 declare(strict_types=1);
-	class DWIPSThermostaticController extends IPSModule
-	{
-		public function Create()
-		{
-			//Never delete this line!
-			parent::Create();
-		}
 
-		public function Destroy()
-		{
-			//Never delete this line!
-			parent::Destroy();
-		}
+class DWIPSThermostaticController extends IPSModule
+{
+    public function Create()
+    {
+        //Never delete this line!
+        parent::Create();
 
-		public function ApplyChanges()
-		{
-			//Never delete this line!
-			parent::ApplyChanges();
-		}
-	}
+        // Properties
+
+        // Attributes
+
+        // Variables
+
+        $this->MaintainVariableInteger("OutputValue", "Output Value", "~Intensity.100", 1);
+
+        $this->MaintainVariableFloat('TargetTemp', ' Target Temp', '~Temperature', 2);
+        $this->EnableAction('TargetTemp');
+
+        $this->MaintainVariableFloat('ActualTemp', ' Actual Temp', '~Temperature', 3);
+        $this->EnableAction('ActualTemp');
+    }
+
+    public function Destroy()
+    {
+        //Never delete this line!
+        parent::Destroy();
+    }
+
+    public function ApplyChanges()
+    {
+        //Never delete this line!
+        parent::ApplyChanges();
+    }
+}
