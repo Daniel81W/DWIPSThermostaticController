ce6e42c8006bdf68fb4ba312c7362a23c53c5bae
Daniel81W <it@dan-web.de>
2024-11-29 19:21:38 -0500

    Upd

--- DWIPSThermostaticController/module.php
+++ DWIPSThermostaticController/module.php
@@ -9,6 +9,17 @@ class DWIPSThermostaticController extends IPSModule
         //Never delete this line!
         parent::Create();
 
+        // Profiles
+        $profilename = "DWIPS" . $this->Translate("HVACMode");
+        if(!IPS_VariableProfileExists($profilename)){
+            IPS_CreateVariableProfile($profilename, 1);
+            IPS_SetVariableProfileValues($profilename, 0, 4, 1);
+            IPS_SetVariableProfileAssociation($profilename, 0, $this->Translate("Automatic"), "Clock", -1);
+            IPS_SetVariableProfileAssociation($profilename, 1, $this->Translate("Comfort"), "Presence-100", -1);
+            IPS_SetVariableProfileAssociation($profilename, 2, $this->Translate("Standby"), "Presence-0", -1);
+            IPS_SetVariableProfileAssociation($profilename, 3, $this->Translate("Economy"), "Moon", -1);
+            IPS_SetVariableProfileAssociation($profilename, 4, $this->Translate("Building Protection"), "Warning", -1);
+        }
         // Properties
         $this->RegisterPropertyInteger("TargetTempVarID",0);
         $this->RegisterPropertyInteger("ActualTempVarID", 0);
@@ -24,6 +35,9 @@ class DWIPSThermostaticController extends IPSModule
 
         $this->MaintainVariable('ActualTemp', $this->Translate('Actual Temp'), 2, '~Temperature', 3, true);
         $this->EnableAction('ActualTemp');
+
+        $this->MaintainVariable('HVACMode', $this->Translate('HVAC Mode'), 1, "DWIPS" . $this->Translate("HVACMode"), 4, true);
+        $this->EnableAction('HVACMode');
     }
 
     public function Destroy()
