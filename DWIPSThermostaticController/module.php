9e657715decb65c141a2a31752b022f52fba49e6
Daniel81W <it@dan-web.de>
2024-12-02 01:44:41 -0500

    Upd

--- DWIPSThermostaticController/module.php
+++ DWIPSThermostaticController/module.php
@@ -26,9 +26,9 @@ class DWIPSThermostaticController extends IPSModule
         $this->RegisterPropertyInteger("ActualTempVarID", 0);
         $this->RegisterPropertyInteger("WindowStateVarID", 0);
         $this->RegisterPropertyInteger("HeatingSystemOperationModeVarID", 0);
-        $this->RegisterPropertyInteger("Kp", 0);
-        $this->RegisterPropertyInteger("Ki", 0);
-        $this->RegisterPropertyInteger("Kd", 0);
+        $this->RegisterPropertyFloat("Kp", 0);
+        $this->RegisterPropertyFloat("Ki", 0);
+        $this->RegisterPropertyFloat("Kd", 0);
 
         // Attributes ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
